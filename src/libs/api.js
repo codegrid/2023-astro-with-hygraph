901a2ca0880eb1a9747c1e2b2c17ddffbd3a4ff9
cancer <zodiac.cancer.j6@gmail.com>
2023-06-13 12:54:19 +0900

    read cookie in dummy api

--- src/libs/api.js
+++ src/libs/api.js
@@ -2,7 +2,16 @@ export const fetchAccount = () => {
   return new Promise((resolve) => {
     setTimeout(() => {
       console.log("api respond");
-      resolve({ name: "cancer" })
+
+      // cookieにログイン情報があったら返す
+      if (
+        document.cookie.includes(
+          "cgdemo-2023-astro-with-hygraph"
+        )
+      )
+        return resolve({ name: "cancer" });
+
+      resolve(null);
     }, 500);
   });
 };
